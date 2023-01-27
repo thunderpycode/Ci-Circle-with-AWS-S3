8327ffb48ce977949c8c781c875f42ca5423d73e
thunderpycode <114567270+scriptwithabhi@users.noreply.github.com>
2023-01-27 15:05:50 +0530

    develop merge

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          I am an Example App Running in the Cloud1
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
